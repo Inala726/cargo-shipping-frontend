c2c936b4be1bc0f41ee911a6ef40f85ad2ef6c68
Inala <inalajacob740@gmail.com>
2025-01-15 09:48:27 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Herosection from "./components/Herosection";
+
+function App() {
+  return (
+    <>
+      <Herosection />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Herosection.tsx
@@ -0,0 +1,14 @@
+import herosectionImage from "../assets/images/image_fx_ (99).jpg";
+
+
+const Nav = () => {
+  return (
+    <>
+        <div className="">
+          <img src={herosectionImage} alt="" />
+        </div>
+    </>
+  );
+};
+
+export default Nav;
